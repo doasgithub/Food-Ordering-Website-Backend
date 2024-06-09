d061d8e1f4bb935b57e654d0a5b4afdfcc44721d
doasgithub <doas5978@gmail.com>
2024-06-09 09:04:59 +0530

    first commit

---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyRestaurantRequest } from "../middlewares/validation";
+
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
+
+router.post("/", 
+     upload.single("imageFile"), 
+     validateMyRestaurantRequest,
+     jwtCheck,
+     jwtParse,
+      MyRestaurantController.createMyRestaurant 
+
+);
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+  
+
+export default router;
