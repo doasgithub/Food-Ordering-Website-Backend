a44dbf2580f774abfc7dcf0919e582e6ffd3c2bf
doasgithub <doas5978@gmail.com>
2024-06-13 09:35:33 +0530

    added orede status and payment page

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoute"
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
  .connect(process.env.MONGODB_CONNECTION as string)
@@ -20,8 +21,14 @@ mongoose
 
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+
 
 app.get("/health", async (req: Request, res: Response) => {
     res.send({ message: "health OK!" });
@@ -30,6 +37,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute)
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000,()=>{
     console.log("server started at 7000")
